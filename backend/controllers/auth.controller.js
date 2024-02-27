22588c6e44146686bdaa62f6336fe4042f05c5c8
Kunal Das <kunal.dusk@gmail.com>
2024-02-27 23:54:00 +0530

    docs: add troubleshooting guide

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -41,7 +41,7 @@ export const signup = async (req, res) => {
 		res.cookie("jwt-linkedin", token, {
 			httpOnly: true, // prevent XSS attack
 			maxAge: 3 * 24 * 60 * 60 * 1000,
-			sameSite: "strict", // prevent CSRF attacks,
+			sameSite: "none",
 			secure: process.env.NODE_ENV === "production", // prevents man-in-the-middle attacks
 		});
 
@@ -81,7 +81,7 @@ export const login = async (req, res) => {
 		await res.cookie("jwt-linkedin", token, {
 			httpOnly: true,
 			maxAge: 3 * 24 * 60 * 60 * 1000,
-			sameSite: "strict",
+			sameSite: "none",
 			secure: process.env.NODE_ENV === "production",
 		});
 
