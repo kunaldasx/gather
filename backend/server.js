445e4f86f086009510e615da4dace394695f663b
Kunal Das <kunal.dusk@gmail.com>
2024-02-27 21:53:00 +0530

    style: apply code formatting

--- backend/server.js
+++ backend/server.js
@@ -19,14 +19,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
-	app.use(
-		cors({
-			origin: process.env.CLIENT_URL,
-			credentials: true,
-		})
-	);
-}
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL,
+		credentials: true,
+	})
+);
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
@@ -40,6 +38,7 @@ app.use("/api/v1/connections", connectionRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
+
 	app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 	app.use((req, res) => {
@@ -51,6 +50,62 @@ app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 	connectDB();
 });
+
+
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import path from "path";
+
+// import authRoutes from "./routes/auth.route.js";
+// import searchRoutes from "./routes/search.route.js";
+// import userRoutes from "./routes/user.route.js";
+// import postRoutes from "./routes/post.route.js";
+// import notificationRoutes from "./routes/notification.route.js";
+// import connectionRoutes from "./routes/connection.route.js";
+
+// import { connectDB } from "./lib/db.js";
+
+// dotenv.config();
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+// const __dirname = path.resolve();
+
+// if (process.env.NODE_ENV !== "production") {
+// 	app.use(
+// 		cors({
+// 			origin: process.env.CLIENT_URL,
+// 			credentials: true,
+// 		})
+// 	);
+// }
+
+// app.use(express.json({ limit: "5mb" }));
+// app.use(cookieParser());
+
+// app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/search", searchRoutes);
+// app.use("/api/v1/users", userRoutes);
+// app.use("/api/v1/posts", postRoutes);
+// app.use("/api/v1/notifications", notificationRoutes);
+// app.use("/api/v1/connections", connectionRoutes);
+
+
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+// 	app.use((req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend/dist/index.html"));
+// 	});
+// }
+
+// app.listen(PORT, () => {
+// 	console.log(`Server running on port ${PORT}`);
+// 	connectDB();
+// });
 
 
 // if (process.env.NODE_ENV === "production") {
